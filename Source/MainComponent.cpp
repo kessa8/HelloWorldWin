54ef83cfd435227908fcd527e302c1fbb982e2d5
Kessa <kessamoore8@gmail.com>
2023-07-08 10:50:57 -0600

    Added Toggle Button

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -4,7 +4,7 @@
 MainComponent::MainComponent()
 {
     
-
+    addAndMakeVisible(toggleButton);
     setSize (600, 400);
 }
 
@@ -29,4 +29,5 @@ void MainComponent::resized()
     // If you add any child components, this is where you should
     // update their positions.
  
+    toggleButton.setBounds(10, 10, 30, 30);
 }
